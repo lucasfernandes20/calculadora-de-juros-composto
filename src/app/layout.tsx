3c310ce8f5bb52e06f91efd382c3178754cc5e8f
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2024-11-03 19:48:44 -0300

    First commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -33,3 +33,4 @@ export default function RootLayout({
     </html>
   );
 }
+
