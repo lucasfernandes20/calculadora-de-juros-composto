7d29fd8c4987bbdbfb050ee22e8f67765f0b6b5f
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2024-11-30 06:10:47 -0300

    feat: update metadata and improve UI components for better user experience

--- src/models/header.tsx
+++ src/models/header.tsx
@@ -15,13 +15,13 @@ const Header: React.FC = () => {
   const { setTheme } = useTheme();
 
   return (
-    <header className="border-b-[1px] border-muted shadow-md mb-6">
-      <div className="container py-2 flex items-center justify-between">
+    <header className="border-b-[1px] border-muted shadow-sm mb-6">
+      <div className="container py-3 flex items-center justify-between">
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="outline" size="icon" className="relative">
-              <Sun className="absolute h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-              <Moon className="h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <Sun className="absolute h-4 w-4 rotate-0 scale-100 transform transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="h-4 w-4 rotate-90 scale-0 transform transition-all dark:rotate-0 dark:scale-100" />
               <span className="sr-only">Toggle theme</span>
             </Button>
           </DropdownMenuTrigger>
@@ -37,7 +37,7 @@ const Header: React.FC = () => {
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
-        <h1 className="text-xl">Capital Rico</h1>
+        <h1 className="text-3xl">Capital Rico</h1>
         <a
           href="https://www.youtube.com/@capitalrico"
           target="_blank"
